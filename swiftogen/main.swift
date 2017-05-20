e2b8f9f9c5c772be4da8ee4d19f322cbd6700d3b
Mikhail Zinov <zinov.m.v@gmail.com>
2017-05-20 07:06:39 +0300

    Change program command line interface

--- swiftogen/main.swift
+++ swiftogen/main.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+let typeKey = CommandLine.arguments[1]
 let filename = CommandLine.arguments[2]
 let filePath = CommandLine.arguments[3]
 let name = ((filename.components(separatedBy: "/").last)?.components(separatedBy: ".").first)!
@@ -16,9 +17,25 @@ let parser = Parser(scanner: Scanner(fileName: filename))
 
 do {
     try parser.parse()
-    let mappingGenerator = MappingGenerator(parser: parser)
-    var s = mappingGenerator.generate()
-    mappingGenerator.touch(code: s, filePath: filePath, fileName: name)
+    let generator: Generator!
+    if typeKey == "-m" {
+        generator = MappingGenerator(parser: parser)
+        generator.touch(code: generator.generate(),
+                               filePath: filePath,
+                               fileName: name,
+                               suffix: mappingSuffix)
+        print("Create file \(name + mappingSuffix).swift")
+    } else if typeKey == "-t" {
+        generator = MappingTransformerGenerator(parser: parser)
+        generator.touch(code: generator.generate(),
+                               filePath: filePath,
+                               fileName: name,
+                               suffix: transformerSuffix)
+        print("Create file \(name + transformerSuffix).swift")
+    } else {
+        throw ArgumentError(message: "", index: 1)
+    }
+    
     
 } catch {
     print((error as! ParserError).localizedDescription + " at file \(name)")
